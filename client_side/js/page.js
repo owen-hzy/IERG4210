2f41437ad25ec4ed3e089d5b5e63570773520014
Owen HUANG <huang_zhengyuan@qq.com>
2013-12-08 01:19:36 +0800

    half comlete #4

--- client_side/js/page.js
+++ client_side/js/page.js
@@ -2,7 +2,7 @@
 	function navbar() {
 		myLib.get2({action:'cat_fetchall'}, function(json){
 			for (var listItems = [], i = 0, cat; cat = json[i]; i++) {
-				(listItems.push('<li id="cat' , parseInt(cat.catid) , '">'), listItems.push('<a href="/' + cat.catid + '-f' + cat.name + '"> ' + cat.name + ' </a>'), listItems.push('</li>'));
+				(listItems.push('<li id="cat' , parseInt(cat.catid) , '">'), listItems.push('<a href="/' + cat.catid + '-' + cat.name + '"> ' + cat.name + ' </a>'), listItems.push('</li>'));
 			}
 			el('navbar').innerHTML = listItems.join('');
 		});		
